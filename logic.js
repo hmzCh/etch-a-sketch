88d165bf681ec8fac01720452585a9c2ba93bcad
Hamza <114156862+hmzCh@users.noreply.github.com>
2022-11-01 13:23:26 +0000

    Create function to make a grid

--- logic.js
+++ logic.js
@@ -1,29 +1,32 @@
 const body = document.querySelector("body")
 
-const grid = document.createElement("div")
-grid.id = "grid"
-body.classList.add("grid")
-body.appendChild(grid)
-
-for (i=1; i<=256; i++) {
-    const square = document.createElement("div")
-    square.style.height = "50px"
-    square.style.width = "50px"
-    square.style.backgroundColor = "grey"
-    square.style.margin = 0
-    square.style.border = 0
-    square.addEventListener('mouseover', () => {square.style.backgroundColor = "black"})
-    grid.classList.add("square")
-    grid.appendChild(square)
-}
+createGrid(16)
 
 const setGridButton = document.createElement("button")
 setGridButton.textContent = "Change grid size"
 body.classList.add("setGridButton")
 body.appendChild(setGridButton)
-setGridButton.addEventListener('click', () => {changeGridSize()});
+setGridButton.addEventListener('click', () => {createGrid(prompt("Enter the number of squares you want per side"))});
 
-function changeGridSize() {
-    const size = prompt("Enter the number of squares you want per side");
-    
+function createGrid(size){
+
+    const grid = document.createElement("div")
+    grid.id = "grid"
+    body.classList.add("grid")
+    body.appendChild(grid)
+
+    squareSize = 50
+
+    for (i=1; i<=size**2; i++) {
+        const square = document.createElement("div")
+        square.style.height = `${(squareSize)}px`
+        square.style.width = `${(squareSize)}px`
+        square.style.backgroundColor = "grey"
+        square.style.margin = 0
+        square.style.border = 0
+        square.addEventListener('mouseover', () => {square.style.backgroundColor = "black"})
+        grid.classList.add("square")
+        grid.appendChild(square)
+    }
+
 }
