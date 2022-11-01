92b34513db5704cb773dd02921eabd8dd9b751f0
Hamza <114156862+hmzCh@users.noreply.github.com>
2022-11-01 12:38:12 +0000

    Add grid

--- logic.js
+++ logic.js
@@ -1,14 +1,20 @@
 const body = document.querySelector("body")
 
-const theGrid = document.createElement("div")
-body.classList.add("theGrid")
-body.appendChild(theGrid)
+const grid = document.createElement("div")
+grid.id = "grid"
+body.classList.add("grid")
+body.appendChild(grid)
 
 const squares = new Array(16)
 
-for (i=1; i<=16; i++) {
+for (i=1; i<=256; i++) {
     const square = document.createElement("div")
-    theGrid.classList.add("square")
-    theGrid.appendChild(square)
+    square.style.height = "50px"
+    square.style.width = "50px"
+    square.style.backgroundColor = "grey"
+    square.style.margin = 0
+    square.style.border = 0
+    grid.classList.add("square")
+    grid.appendChild(square)
     squares.push(square)
 }
